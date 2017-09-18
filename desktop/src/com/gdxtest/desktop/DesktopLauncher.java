1893b7a19783624c7a541a4d0a793a2a70173c97
tkoehler <thom.koehler@gmail.com>
2017-09-18 20:28:18 +0200

    change to Kotlin

--- desktop/src/com/gdxtest/desktop/DesktopLauncher.java
+++ desktop/src/com/gdxtest/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.gdxtest.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.gdxtest.GdxTest;
+import com.gdxtest.GdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxTest(), config);
+		new LwjglApplication(new GdxGame(), config);
 	}
 }
